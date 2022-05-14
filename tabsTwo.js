bb8a5bfa7dbb849cb30df57477ff728f5a19a6f5
LAPTOP-G3EJ2PGP\patil <patilno7@gmail.com>
2022-05-14 17:52:22 -0400

    New tabs

---
+++ tabsTwo.js
@@ -0,0 +1,16 @@
+
+function init() {
+
+var getslide = $('.main-box li').length - 1;
+
+var slidecal = 30/getslide+'%';
+
+$('.box').css({"width": slidecal});
+
+$('.box').click(function(){
+    $(this).siblings().removeClass('active');
+   $(this).addClass('active');
+});
+
+};
+
